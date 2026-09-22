c53df2f9306367c8af827a49d46e3aaffc909d62
meiersc <meiersc19@gmail.com>
2026-09-21 20:42:10 -0400

    yay

---
+++ src/EvenorOdd.java
@@ -0,0 +1,12 @@
+
+    public static void main(String[] args) {
+        // Hardcoded input value
+        int numToExamine = 2;
+
+        // Use modulo to check if the number is divisible by 2
+        if (numToExamine % 2 == 0) {
+            System.out.println(numToExamine + " is Even.");
+        } else {
+            System.out.println(numToExamine + " is Odd.");
+        }
+    }
